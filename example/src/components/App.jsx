82b29df8026f9493a187b825b8a89221ca9d6c17
Laurie Jones <hello@lauriejones.me>
2017-03-08 12:01:42 +1100

    SIP-1235 - working component, plus lines in-between options

--- example/src/components/App.jsx
+++ example/src/components/App.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import ReactScaleSlider from '@nib-components/react-scale-slider';
+import Theme from '@nib-components/theme';
+import ScaleSlider from '@nib-components/react-scale-slider';
 
-const App = () => (
-  <div>
-    <ReactScaleSlider/>
-  </div>
-);
+const options = {
+  'Single': 'Me',
+  'Couple': 'My partner and me',
+  'Family': 'My partner, our kids and me',
+  'SingleParentFamily': 'My kids and me'
+};
+
+class App extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      scale: 'Single'
+    };
+
+    this.handleScaleChange = this.handleScaleChange.bind(this);
+  }
+
+  handleScaleChange(option) {
+    this.setState({scale: option});
+  }
+
+  render() {
+    return (
+      <Theme>
+        <ScaleSlider
+          value={this.state.scale}
+          options={options}
+          onChange={this.handleScaleChange}
+        />
+      </Theme>
+    );
+  }
+
+}
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,46 @@ import React from 'react';
 import styled from 'styled-components';
 import {Margin, Padding, p} from 'styled-components-spacing';
 import Grid from 'styled-components-grid';
-import Theme from '@nib-components/theme';
 import colors from '@nib-styles/colors';
 
 const Wrapper = styled.div`
   background-color: ${colors.sneezy};
+  text-align: center;
+`;
+
+const IconWrapper = styled.div`
+  position: relative;
+
+  &:before,
+  &:after {
+    display: block;
+    height: 2px;
+    position: absolute;
+    top: 50%;
+    background-color: ${colors.grumpy};
+    transform: translateY(-50%);
+  }
+
+  &:before {
+    ${({first}) => !first && 'content: \'\';'}
+    left: 0;
+    right: 50%;
+  }
+
+  &:after {
+    ${({last}) => !last && 'content: \'\';'}
+    left: 50%;
+    right: 0;
+  }
 `;
 
 const IconButton = styled.button`
   ${p(0)}
-  background-color: ${colors.white};
+  background-color: ${colors.sneezy};
   color: ${props => props.selected ? colors.elizabeth : colors.doc};
   border: none;
+  position: relative;
+  z-index: 1;
 `;
 
 const Label = styled.label`
@@ -23,66 +51,54 @@ const Label = styled.label`
   text-transform: uppercase;
 `;
 
-const options = {
-  'Single': 'Me',
-  'Couple': 'My partner and me',
-  'Family': 'My partner, our kids and me',
-  'SingleParentFamily': 'My kids and me'
-};
-
 export default class ScaleSlider extends React.Component {
 
   constructor(props) {
     super(props);
-
-    this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange(option) {
-    this.props.value = option;
-  }
-
   handleClick(option) {
-    this.props.onChange(option);
+    return () => this.props.onChange(option);
   }
 
   render() {
-    const {value, onFocus, onChange, onBlur, autoFocus, ...otherProps} = this.props;
+    const {value, options, autoFocus} = this.props;
     return (
-      <Theme onChange={this.handleChange}>
-        <Wrapper>
-          <Padding all={{xs: '3', md: '4'}}>
+      <Wrapper>
+        <Padding all={{xs: '3', md: '4'}}>
 
-            <Grid>
-              {Object.keys(options).map(option => {
-                const selected = option === value;
-                return (
+          <Grid>
+            {Object.keys(options).map((option, index) => {
+              const selected = option === value;
+              return (
 
-                <Grid.Unit width={1/Object.keys(options).length} key={option}>
-                  <IconButton
-                    onClick={this.handleClick(option)}
-                    selected={value === option}
-                    autoFocus={autoFocus && this.props.value === option}
-                  >
-                    <Padding all={3}>
-                      {option}
-                    </Padding>
-                  </IconButton>
+                <Grid.Unit width={1 / Object.keys(options).length} key={option}>
+                  <IconWrapper first={index === 0} last={index === Object.keys(options).length - 1}>
+                    <IconButton
+                      onClick={this.handleClick(option)}
+                      selected={selected}
+                      autoFocus={autoFocus && selected}
+                    >
+                      <Padding all={3}>
+                        {option}
+                      </Padding>
+                    </IconButton>
+                  </IconWrapper>
                 </Grid.Unit>
-                )}
-              )}
-            </Grid>
 
-            <Margin top={3}>
-              <Label htmlFor="scale">
-                {options[value]}
-              </Label>
-            </Margin>
+              );
+            })}
+          </Grid>
 
-          </Padding>
-        </Wrapper>
-      </Theme>
+          <Margin top={3}>
+            <Label htmlFor="scale">
+              {options[value]}
+            </Label>
+          </Margin>
+
+        </Padding>
+      </Wrapper>
     );
   }
 }
@@ -93,6 +109,5 @@ ScaleSlider.propTypes = {
 };
 
 ScaleSlider.defaultProps = {
-  value: 'Single',
   onChange: () => {/*do nothing*/}
 };
